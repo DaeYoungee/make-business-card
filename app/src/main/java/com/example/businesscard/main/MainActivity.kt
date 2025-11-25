f414f4e89a924aa44a06e06eeadd2f39d4a16905
DaeYoung Jeong <eodud1025@naver.com>
2025-11-25 20:52:09 +0900

    ResultActivity 이동 함수 구현

--- app/src/main/java/com/example/businesscard/main/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/main/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.businesscard.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.example.businesscard.main.component.MainScreen
-import com.example.businesscard.ui.theme.MyApplicationTheme
+import com.example.businesscard.main.ui.theme.MyApplicationTheme
+import com.example.businesscard.result.ResultActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,11 +23,20 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.Companion.fillMaxSize()) {
-                    MainScreen()
-
+                Scaffold(modifier = Modifier.Companion.fillMaxSize()) { innerPadding ->
+                    Box(modifier = Modifier.fillMaxSize().padding(innerPadding)) {
+                        MainScreen(startResultActivity = ::startResultActivity)
+                    }
                 }
             }
         }
+    }
+
+    fun startResultActivity(name: String, phone: String) {
+        val intent = Intent(this, ResultActivity::class.java).apply {
+            putExtra("name", name)
+            putExtra("phone", phone)
+        }
+        startActivity(intent)
     }
 }
